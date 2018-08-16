35d33d20d0c429cfae30a417d4389ac2a6d1a2fb
kurtw29 <ckurt37@gmail.com>
2018-08-15 18:51:56 -0700

    updatae loginReg

--- loginReg/urls.py
+++ loginReg/urls.py
@@ -1,4 +1,4 @@
-"""Django_Template URL Configuration
+"""loginReg URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
--- loginReg/wsgi.py
+++ loginReg/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Django_Template project.
+WSGI config for loginReg project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django_Template.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "loginReg.settings")
 
 application = get_wsgi_application()
